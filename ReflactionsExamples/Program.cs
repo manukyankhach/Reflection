9e9c00cebb05376e393621cda63e5574135cfddb
manukyankhach <83696364+manukyankhach@users.noreply.github.com>
2022-05-30 11:49:52 +0400

    GetMethods

--- ReflactionsExamples/Program.cs
+++ ReflactionsExamples/Program.cs
@@ -12,77 +12,93 @@ namespace ReflactionsExamples
         static void Main(string[] args)
         {
             //Получение типа через typeof:
-            Type type = typeof(Person);
-            Type type1 = typeof(Program);
-            Console.WriteLine($"typeof - {type}");
-            Console.WriteLine($"typeof - {type1}");
-            Console.WriteLine("------------------------------");
+
+            //Type type = typeof(Person);
+            //Type type1 = typeof(Program);
+            //Console.WriteLine($"typeof - {type}");
+            //Console.WriteLine($"typeof - {type1}");
+            //Console.WriteLine("------------------------------");
 
             //Получение типа через GetType:
-            Person person = new Person("Jon", 35);
-            Program program = new Program();
-            Type type2 = person.GetType();
-            Console.WriteLine($"GetType - {type2}");
-            Console.WriteLine($"GetType - {program}");
-            Console.WriteLine("------------------------------");
+
+            //Person person = new Person("Jon", 35);
+            //Program program = new Program();
+            //Type type2 = person.GetType();
+            //Console.WriteLine($"GetType - {type2}");
+            //Console.WriteLine($"GetType - {program}");
+            //Console.WriteLine("------------------------------");
 
             //Получение типа через Type.GetType():
-            Type type3 = Type.GetType("ReflactionsExamples.Person", false, true);
-            Type type4 = Type.GetType("ReflactionsExamples.Program", false, true);
-            Console.WriteLine($"Type.GetType() - {type3}");
-            Console.WriteLine($"Type.GetType() - {type4}");
-            Console.WriteLine("------------------------------");
+
+            //Type type3 = Type.GetType("ReflactionsExamples.Person", false, true);
+            //Type type4 = Type.GetType("ReflactionsExamples.Program", false, true);
+            //Console.WriteLine($"Type.GetType() - {type3}");
+            //Console.WriteLine($"Type.GetType() - {type4}");
+            //Console.WriteLine("------------------------------");
 
             //Консольный вывод:
-            Console.WriteLine($"type.Name - {type.Name}");
-            Console.WriteLine($"type.FullName - {type.FullName}");
-            Console.WriteLine($"type.Namespace - {type.Namespace}");
-            Console.WriteLine($"type.Assembly - {type.Assembly}");
-            Console.WriteLine($"type.AssemblyQualifiedName - {type.AssemblyQualifiedName}");
-            Console.WriteLine($"type.IsValueType - {type.IsValueType}");
-            Console.WriteLine($"type.IsClass - {type.IsClass}");
-            Console.WriteLine("------------------------------");
+
+            //Console.WriteLine($"type.Name - {type.Name}");
+            //Console.WriteLine($"type.FullName - {type.FullName}");
+            //Console.WriteLine($"type.Namespace - {type.Namespace}");
+            //Console.WriteLine($"type.Assembly - {type.Assembly}");
+            //Console.WriteLine($"type.AssemblyQualifiedName - {type.AssemblyQualifiedName}");
+            //Console.WriteLine($"type.IsValueType - {type.IsValueType}");
+            //Console.WriteLine($"type.IsClass - {type.IsClass}");
+            //Console.WriteLine("------------------------------");
 
             //Поиск реализованных интерфейсов
-            Type type5 = typeof(Person);
-            Type [] getInterfaces = type5.GetInterfaces();
-            foreach (Type item in getInterfaces)
-            {
-                Console.WriteLine($"Interfaces Items - {item}");
-            }
-            Console.WriteLine("------------------------------");
 
-            Type type6 = typeof(Person);
-            MemberInfo[] memberInfo = type6.GetMembers();
-            foreach (MemberInfo item in memberInfo)
-            {
-                Console.WriteLine($"item.Name - {item.Name},item.DeclaringType - {item.DeclaringType},item.MemberType - {item.MemberType}," +
-                    $"item.MetadataToken - {item.MetadataToken}, item.CustomAttributes -{item.CustomAttributes}");
-            }
-            Console.WriteLine("------------------------------");
+            //Type type5 = typeof(Person);
+            //Type [] getInterfaces = type5.GetInterfaces();
+            //foreach (Type item in getInterfaces)
+            //{
+            //    Console.WriteLine($"Interfaces Items - {item}");
+            //}
+            //Console.WriteLine("------------------------------");
 
-            Type type7 = Type.GetType("ReflactionsExamples.Person", false, true);
-            var memberInfo2 = type7.GetMembers();
-            Console.WriteLine($"memberInfo2[0].Name - {memberInfo2[0].Name}, memberInfo2[0].DeclaringType - {memberInfo2[0].DeclaringType}," +
-                $"memberInfo2[0].MemberType - {memberInfo2[0].MemberType}");
-            Console.WriteLine("------------------------------");
+            //Type type6 = typeof(Person);
+            //MemberInfo[] memberInfo = type6.GetMembers();
+            //foreach (MemberInfo item in memberInfo)
+            //{
+            //    Console.WriteLine($"item.Name - {item.Name},item.DeclaringType - {item.DeclaringType},item.MemberType - {item.MemberType}," +
+            //        $"item.MetadataToken - {item.MetadataToken}, item.CustomAttributes -{item.CustomAttributes}");
+            //}
+            //Console.WriteLine("------------------------------");
 
-            Person person1 = new Person("Gag", 25);
-            Type type8 = person1.GetType();
-            MemberInfo[] memberInfo1 = type8.GetMembers(BindingFlags.DeclaredOnly
-            | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
-            foreach (var item in memberInfo1)
-            {
-                Console.WriteLine(item);
-            }
+            //GetMembers()
 
-            Console.WriteLine("------------------------------");
+            //Type type7 = Type.GetType("ReflactionsExamples.Person", false, true);
+            //var memberInfo2 = type7.GetMembers();
+            //Console.WriteLine($"memberInfo2[0].Name - {memberInfo2[0].Name}, memberInfo2[0].DeclaringType - {memberInfo2[0].DeclaringType}," +
+            //    $"memberInfo2[0].MemberType - {memberInfo2[0].MemberType}");
+            //Console.WriteLine("------------------------------");
+
+            //Person person1 = new Person("Gag", 25);
+            //Type type8 = person1.GetType();
+            //MemberInfo[] memberInfo1 = type8.GetMembers(BindingFlags.DeclaredOnly
+            //| BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+            //foreach (var item in memberInfo1)
+            //{
+            //    Console.WriteLine(item);
+            //}
+
+            //Console.WriteLine("------------------------------");
+
             // получаем метод print
-            MemberInfo[] memberInfos = type8.GetMember("Print", BindingFlags.Instance | BindingFlags.Public);
-            foreach (var item in memberInfos)
-            {
-                Console.WriteLine($"{item.MemberType} - {item.Name}");
-            }
+
+            //MemberInfo[] memberInfos = type8.GetMember("Print", BindingFlags.Instance | BindingFlags.Public);
+            //foreach (var item in memberInfos)
+            //{
+            //    Console.WriteLine($"{item.MemberType} - {item.Name}");
+            //}
+            //Console.WriteLine("------------------------------");
+
+            // GetMethods()
+            Type getMethods = typeof(Person);
+            MethodInfo[] methodInfo = getMethods.GetMethods();
+            Console.WriteLine($"IsFamily - {methodInfo[0].IsFamily}, IsFamilyOrAssembly - {methodInfo[1].IsFamilyOrAssembly}," +
+                $" IsPrivate - {methodInfo[2].IsPrivate}, IsPublic - {methodInfo[3].IsPublic}, Name - {methodInfo[4].Name}");
             Console.ReadLine();
         }
     }
